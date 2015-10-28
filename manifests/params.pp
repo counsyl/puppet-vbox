58606b8e51795edfadb83a8e0d60037a571461b4
Justin Bronn <justin@counsyl.com>
2015-10-28 17:30:01 -0500

    Update default VirtualBox version to 4.3.32.

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,11 +4,11 @@
 #
 class vbox::params {
   # The full version, and the major version (omits the subminor number).
-  $version = hiera('vbox::version', '4.3.30')
+  $version = hiera('vbox::version', '4.3.32')
   $major_version = inline_template("<%= @version.split('.')[0..1].join('.') %>")
 
   # The build number is necessary to construct proper URLs.
-  $build = hiera('vbox::build', '101610')
+  $build = hiera('vbox::build', '103443')
 
   # The source of the GPG key for the VirtualBox apt repository.
   $gpg_source = 'puppet:///modules/vbox/oracle_vbox.gpg'
